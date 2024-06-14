8fdad9f04ad440aab982f393a1dd47b87a7d5bf5
Victoria <victoriabotelho14@gmail.com>
2024-06-14 15:34:38 -0300

    feat: implementa método para addConta

--- projetos/BanKOO/src/main/java/com/bankoo/Banco.java
+++ projetos/BanKOO/src/main/java/com/bankoo/Banco.java
@@ -1,12 +1,22 @@
 package com.bankoo;
 
 import java.util.List;
+import java.util.ArrayList;
 import lombok.Data;
 
 @Data
 public class Banco {
 
 	private String nome;
-	private List<Conta> contas;
+	private List<Conta> contas = new ArrayList<>();
+
+	public void adicionarConta(Conta conta) {
+		for (Conta c : contas) {
+			if (c.getCliente().equals(conta.getCliente()) && c.getClass().equals(conta.getClass())) {
+				throw new RuntimeException("Cliente já possui este tipo de conta no banco");
+			}
+		}
+		contas.add(conta);
+	}
 
 }
