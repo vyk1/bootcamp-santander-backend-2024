b0dcb638045f37fe906f497fb844491ce76a352f
Victoria <victoriabotelho14@gmail.com>
2024-06-25 14:48:34 -0300

    mil: adiciona desafio 1 stream api

---
+++ projetos/StreamAPI/src/DesafioUm.java
@@ -0,0 +1,10 @@
+public class DesafioUm {
+
+    public static void main(String[] args) {
+        // Crie um array de inteiros e ordene o array
+        int[] numeros = { 1, 5, 3, 2, 4 };
+        Arrays.sort(numeros);
+        System.out.println(Arrays.toString(numeros));
+    }
+
+}
